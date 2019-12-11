71995b0722af5dad6b898e27658033c360779ea0
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-12-11 16:40:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash[:yukihiro_matsumoto][:known_for] 
+ programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+ programmer_hash[:yukihiro_matsumoto][:known_for][:languages] = ["LISP", "C"]
     
 end
 
